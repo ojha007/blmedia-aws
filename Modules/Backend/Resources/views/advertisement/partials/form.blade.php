97228d1ef078f63feec6316f8b40046fe29edef4
ojha007 <ojhaamir007@gmail.com>
2020-10-11 23:32:13 +0545

    file manager in advertisements

--- Modules/Backend/Resources/views/advertisement/partials/form.blade.php
+++ Modules/Backend/Resources/views/advertisement/partials/form.blade.php
@@ -44,17 +44,35 @@
             <label for="fieldID4">Banner Picture</label>
             <div class="input-group">
                    <span class="input-group-btn btn-flat">
-                     <a id="banner_image" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                     <button
+                         type="button"
+                         onclick="return openElFinder(event, 'feature_image');"
+                         data-inputid="feature_image"
+                         class="btn btn-primary  ">
                        <i class="fa fa-picture-o"></i> Choose
-                     </a>
+                     </button>
                    </span>
-                <label for="image_label">
-                </label>
-                {{Form::text('image',null,['id'=>'image_label','class'=>'form-control'])}}
+                {{Form::text('image',null,['id'=>'feature_image','class'=>'form-control'])}}
             </div>
-            <img id="holder" style="margin-top:15px;height:100px;" alt=""
-            src="{{isset($advertisement) ? $advertisement->image : ''}}">
+            <img id="holder" style="margin-top:15px;height:100px;width: 250px;" alt=""
+                 src="{{isset($advertisement) ? $advertisement->image : ''}}">
         </div>
+
+{{--        <div class="form-group col-md-12" style="padding-right: 0">--}}
+{{--            <label for="fieldID4">Banner Picture</label>--}}
+{{--            <div class="input-group">--}}
+{{--                   <span class="input-group-btn btn-flat">--}}
+{{--                     <a id="banner_image" data-input="thumbnail" data-preview="holder" class="btn btn-primary">--}}
+{{--                       <i class="fa fa-picture-o"></i> Choose--}}
+{{--                     </a>--}}
+{{--                   </span>--}}
+{{--                <label for="image_label">--}}
+{{--                </label>--}}
+{{--                {{Form::text('image',null,['id'=>'image_label','class'=>'form-control'])}}--}}
+{{--            </div>--}}
+{{--            <img id="holder" style="margin-top:15px;height:100px;" alt=""--}}
+{{--                 src="{{isset($advertisement) ? $advertisement->image : ''}}">--}}
+{{--        </div>--}}
 
         <div class="form-group col-md-12 {{$errors->has('sub_description') ? 'has-error':''}}">
             {{ Form::label('sub_description', 'Sub Description:', ['class'=>' control-label '])}}
@@ -81,7 +99,30 @@
     </div>
 </div>
 @push('scripts')
+    <script type="text/javascript" src="{{asset('/packages/barryvdh/elfinder/js/standalonepopup.min.js')}}"></script>
+    <script>
+        $(document).ready(function () {
+            var routePrefix = '{{$edition}}';
+            window.input_id = '';
+            window.openElFinder = function (event, input_id) {
+                event.preventDefault();
 
+                window.single = true;
+                window.old = false;
+                window.input_id = input_id;
+                let url = '/' + routePrefix + '/bl-secure/elfinder/popup/' + input_id;
+                window.open(url, '_blank', 'scrollbars=yes,height=500,width=1000');
+
+                return false;
+            };
+
+            // function to update the file selected by elfinder
+            window.processSelectedFile = function (filePath, requestingField) {
+                console.log(filePath);
+                $('#' + requestingField).val(filePath).trigger('change');
+            }
+        });
+    </script>
 @endpush
 
 
