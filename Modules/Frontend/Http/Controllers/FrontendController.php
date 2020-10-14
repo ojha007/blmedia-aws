ab220d65ae7111a07a553dddd6fd66f1100af993
ojha007 <ojhaamir007@gmail.com>
2020-10-14 15:28:13 +0545

    removed cache

--- Modules/Frontend/Http/Controllers/FrontendController.php
+++ Modules/Frontend/Http/Controllers/FrontendController.php
@@ -56,7 +56,7 @@ class FrontendController extends Controller
         $blSpecialNews = $newsRepo->getCacheNewsByExtraColumn('is_special', 5);
         $firstPositionNews = $newsRepo->getCacheNews(1, CategoryPositions::FRONT_BODY_POSITION, 9, 'firstPositionNews');
         $fourthPositionNews = $newsRepo->getCacheNews(4, CategoryPositions::FRONT_BODY_POSITION, 5, 'fourthPositionNews');
-        dd($fourthPositionNews);
+//        dd($fourthPositionNews);
         $fifthPositionNews = $newsRepo->getCacheNews(5, CategoryPositions::FRONT_BODY_POSITION, 5, 'fifthPositionNews');
         $sixthPositionNews = $newsRepo->getCacheNews(6, CategoryPositions::FRONT_BODY_POSITION, 6, 'sixthPositionNews');
         $seventhPositionNews = $newsRepo->getCacheNews(7, CategoryPositions::FRONT_BODY_POSITION, 6, 'seventhPositionNews');
