e4972a9a7806ab93578f1160a1c46041b88a7b1a
ojha007 <ojhaamir007@gmail.com>
2020-10-16 17:14:33 +0545

    image in en and hindi

--- Modules/Frontend/Resources/views/components/right-content/right-middle-2/right-middle-2.blade.php
+++ Modules/Frontend/Resources/views/components/right-content/right-middle-2/right-middle-2.blade.php
@@ -2,8 +2,14 @@
                 'class' => 'front_body_position_7',
 
 ])
+@if(request()->segment(1) == 'nepali')
+    @include('frontend::components.news.news-template',['allNews'=>$seventhPositionNews,
+                    'class' => 'front_body_position_7',
+                    'image'=>'reporter_image',
+    ])
+@else
+    @include('frontend::components.news.news-template',['allNews'=>$seventhPositionNews,
+                   'class' => 'front_body_position_7',
 
-@include('frontend::components.news.news-template',['allNews'=>$seventhPositionNews,
-                'class' => 'front_body_position_7',
-                'image'=>'reporter_image',
-])
+   ])
+@endif
