edc15249fa092385d1d3cb52900f1c17d8572ac4
ojha007 <ojhaamir007@gmail.com>
2020-10-17 06:36:34 +0545

    trending news

--- Modules/Frontend/Http/Controllers/FrontendController.php
+++ Modules/Frontend/Http/Controllers/FrontendController.php
@@ -58,7 +58,7 @@ class FrontendController extends Controller
         $anchorNews = $newsRepo->getCacheNewsByExtraColumn('is_anchor', 5);
         $blSpecialNews = $newsRepo->getCacheNewsByExtraColumn('is_special', 5);
 //        dd($blSpecialNews);
-        $trendingNews = [];
+        $trendingNews = (new NewsRepository())->getTrendingNews(5);
         $firstPositionNews = $newsRepo->getCacheNews(1, CategoryPositions::FRONT_BODY_POSITION, 9, 'firstPositionNews');
         $fourthPositionNews = $newsRepo->getCacheNews(4, CategoryPositions::FRONT_BODY_POSITION, 5, 'fourthPositionNews');
         $fifthPositionNews = $newsRepo->getCacheNews(5, CategoryPositions::FRONT_BODY_POSITION, 5, 'fifthPositionNews');
