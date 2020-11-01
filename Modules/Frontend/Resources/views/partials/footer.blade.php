a4557be5ff35a9ebcba5696a89e5b55eea5c49e4
prakash pokhrel <talktoprakashpokhrel@gmail.com>
2020-11-01 22:24:31 +0545

    changes new design

--- Modules/Frontend/Resources/views/partials/footer.blade.php
+++ Modules/Frontend/Resources/views/partials/footer.blade.php
@@ -30,59 +30,21 @@
 
     </div>
 </footer>--}}
-<footer class="mainfooter" role="contentinfo">
+<footer class="main-footer">
     <div class="footer-middle">
         <div class="container">
-            <div class="row">
-                <div class="col-md-3 ">
+            <div class="row pb-2">
+                <div class="col-md-2 my-auto">
                     <div class="footer-pad">
-                    <a href="{{route($routePrefix.'index')}}">
-                        <picture class="brand-logo ">
-                            <img class=" " src="{{asset('frontend/img/logo.png')}}" height="50" width="100"
-                                 alt="BL Media">
-                        </picture>
-                    </a><br>
-                    <ul class="list-unstyled ">
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Contact us</a></li>
-                    </ul>
-                    <ul class="social-network social-circle ">
-                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook"></i></a></li>
-                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                        <li><a href="#" class="icoInstagram" title="Twitter"><i class="fab fa-instagram"></i></a></li>
-                        <li><a href="#" class="icoYoutube" title="Twitter"><i class="fab fa-youtube"></i></a></li>
-                    </ul>
+                        <a href="{{route($routePrefix.'index')}}">
+                            <picture class="brand-logo ">
+                                <img class=" " src="{{asset('frontend/img/logo.png')}}" height="50" width="100"
+                                     alt="BL Media">
+                            </picture>
+                        </a>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-6 ">
-                    <!--Column1-->
-                    <div class="footer-pad">
-                        <h4>Heading 1</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#"></a></li>
-                            <li><a href="#">Payment Center</a></li>
-                            <li><a href="#">Contact Directory</a></li>
-                            <li><a href="#">Forms</a></li>
-                            <li><a href="#">News and Updates</a></li>
-                            <li><a href="#">FAQs</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <!--Column1-->
-                    <div class="footer-pad">
-                        <h4>Heading 2</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Website Tutorial</a></li>
-                            <li><a href="#">Accessibility</a></li>
-                            <li><a href="#">Disclaimer</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">FAQs</a></li>
-                            <li><a href="#">Webmaster</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6 ">
+                <div class="col-md-2 col-sm-6 ">
                     <!--Column1-->
                     <div class="footer-pad ">
                         <h4>Editions</h4>
@@ -90,16 +52,39 @@
                             <li><a href="#">नेपाली</a></li>
                             <li><a href="#">English</a></li>
                             <li><a href="#">Hindi</a></li>
-                            <li><a href="#">Español</a></li>
-                            <li><a href="#">عربي</a></li>
                         </ul>
                     </div>
                 </div>
-
+                <div class="col-md-5 col-sm-6 ">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>About us</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#"></a></li>
+                            <li>Email : breaknlinks@gmai.com</li>
+                            <li>Contact No :+1202-480-1143</li>
+                            <li>Address :<a target="_blank" href="https://www.google.com/maps/place/801+Wayne+Ave+%23202,+Silver+Spring,+MD+20910,+USA/@38.9969765,-77.0247881,17.5z/data=!4m5!3m4!1s0x89b7c8b0075365a1:0xc4d153bfecc068cc!8m2!3d38.9970815!4d-77.0223068"> 801 Wayne Avenue #202,Sliver Spring MD 20910</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6  ">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Connect with us</h4>
+                        <ul class="social-network social-circle">
+                            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook"></i></a>
+                            </li>
+                            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" class="icoInstagram" title="Twitter"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li><a href="#" class="icoYoutube" title="Twitter"><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="row">
                 <div class="col-md-12 copy">
-                    <p class="text-center">&copy; Copyright 2020 - BLMedia.  All rights reserved.</p>
+                    <p class="text-center">&copy; Copyright 2020 - BLMedia. All rights reserved.</p>
                 </div>
             </div>
 
